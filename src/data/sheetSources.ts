518ddf2889c022bbb043a53d6dc3d54718546865
SDW7427 <147566125+SDW7427@users.noreply.github.com>
2026-02-27 11:09:32 +0900

    Update sheetSources.ts

--- src/data/sheetSources.ts
+++ src/data/sheetSources.ts
@@ -1,6 +1,6 @@
 export const SHEET_SOURCES = {
   nodesCsvUrl:
-    'https://docs.google.com/spreadsheets/d/e/2PACX-1vQmxVYnDbPmy4_vXfPGrnGnhn_y7CL-F2kcxOcbqc-e1Gq2oOaGH4xShCB-si0UCts2oKBGFhpyQ06_/pub?gid=2054925201&single=true&output=csv',
+    'https://docs.google.com/spreadsheets/d/e/2PACX-1vQmxVYnDbPmy4_vXfPGrnGnhn_y7CL-F2kcxOcbqc-e1Gq2oOaGH4xShCB-si0UCts2oKBGFhpyQ06_/pub?gid=29728032&single=true&output=csv',
   edgesCsvUrl:
-    'https://docs.google.com/spreadsheets/d/e/2PACX-1vQmxVYnDbPmy4_vXfPGrnGnhn_y7CL-F2kcxOcbqc-e1Gq2oOaGH4xShCB-si0UCts2oKBGFhpyQ06_/pub?gid=1460213672&single=true&output=csv',
+    'https://docs.google.com/spreadsheets/d/e/2PACX-1vQmxVYnDbPmy4_vXfPGrnGnhn_y7CL-F2kcxOcbqc-e1Gq2oOaGH4xShCB-si0UCts2oKBGFhpyQ06_/pub?gid=1363019978&single=true&output=csv',
 } as const;
